01cba262d64953bbc29954c75a282e7cec0223d5
Cameron Billingham <cbillin3@lion.lmu.edu>
2015-02-01 18:21:59 -0800

    Finished chap 3. Have a working stack and call to c function

--- littleos/loader.s
+++ littleos/loader.s
@@ -4,6 +4,16 @@ global loader                   ; the entry symbol for ELF
     FLAGS        equ 0x0            ; multiboot flags
     CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                     ; (magic number + checksum + flags should equal 0)
+    KERNEL_STACK_SIZE equ 4096      ; size of stack in bytes
+
+    NUM equ 8
+
+
+    section .bss
+    align 4                                     ; align at 4 bytes
+    kernel_stack:                               ; label points to beginning of memory
+        resb KERNEL_STACK_SIZE                  ; reserve stack for the kernel
+
 
     section .text:                  ; start of the text (code) section
     align 4                         ; the code must be 4 byte aligned
@@ -12,6 +22,16 @@ global loader                   ; the entry symbol for ELF
         dd CHECKSUM                 ; and the checksum
 
     loader:                         ; the loader label (defined as entry point in linker script)
-        mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
+
+        extern sum_of_three
+
+        mov     esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the stack (end of memory area)
+
+        push    NUM                 ; arg3
+        push    NUM                 ; arg2
+        push    NUM                 ; arg1
+        call    sum_of_three        ; call the function, the result will be in eax
+
+
     .loop:
-        jmp .loop                   ; loop forever-
+        jmp .loop                   ; loop forever
