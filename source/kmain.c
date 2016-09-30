e324324ac18caa66708beb61405fcc608955e63f
Cameron Billingham <cbillin3@lion.lmu.edu>
2016-09-30 13:19:14 -0700

    add ascii text to boot code

--- source/kmain.c
+++ source/kmain.c
@@ -20,7 +20,8 @@
         unsigned int address_of_module = mbinfo->mods_addr;
 
         clear_screen();
-        fb_write_text("Welcome to ARDA!\n");
+        fb_write_text("\n                                    `mMMs`                            \n                                     -dMMm:                           \n                                      `+NMMh:                         \n  `+smmh+`        :hmh-  -ohmmd/        `hMMMd/       .oymmh/         \n     -dMMm-        :MMh:ys:..sMN.     :ymhoomMMm/        :NMMh`       \n     `sMMMN:       /MMN+`    +No`   -dNo`    :dMMy`      .dMMMm.      \n   `+Nh:yMMM/      /MMs   -oho.    :NM/       `sMM/    `sNs:dMMN-     \n  :dm:  `yMMM:     /MMs`:mMm-      yMM/        /Mm.   /Nd-  `dMMm.    \n`yMN-   .oNMMM/    +MMy  :dMMh:`   :NMMy:.`.:odNs`  -hMm`  `-yMMMm-   \n-hNMNddds:`oNMMy: `hMMh    -yNMmo-  .odNMMMNds:`    :dMMNddho-`yMMNs- \n  ``..`     `....  `..`      `.`       ``.``          `...`     ....`\n");
+        fb_write_text("\nWelcome to ARDA!\n");
         serial_write(COM1, "It's working... it's working!");
 
         
